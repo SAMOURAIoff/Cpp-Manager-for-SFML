2bc2357791ea3972e1ae10647a24479039c8c3fb
SAMOURAIoff <116149596+SAMOURAIoff@users.noreply.github.com>
2024-04-23 13:28:12 +0200

    m

--- New_Manager/NEW_DLL/setupDLL.bat
+++
@@ -1,5 +0,0 @@
-copy /Y DLL\*.lib lib\*.lib
-copy /Y DLL\Release\*.dll x64\Release\*.dll
-copy /Y DLL\Debug\*.dll x64\Debug\*.dll
-copy /Y DLL\SFML_ENGINE\*.h include\SFML_ENGINE\*.h
-exit 0
--- New_Manager/autoconfigRelease.bat
+++
@@ -1,7 +0,0 @@
-copy /Y ..\DLL_ENGINE\Release\*.lib ..\lib\*.lib
-del ..\DLL_ENGINE\Release\*lib
-del ..\DLL_ENGINE\Release\*.pdb
-del ..\DLL_ENGINE\Release\*.exp
-copy /Y ..\DLL_ENGINE\Release\*.dll ..\x64\Release\*.dll
-copy /Y *.h ..\include\SFML_ENGINE\*.h
-exit 0
